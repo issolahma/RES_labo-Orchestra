e088a0d158a873b441ebddcba2fe495531188653
issolahma <22448610+issolahma@users.noreply.github.com>
2021-06-10 10:31:36 +0200

    plus mieux

--- docker/image-musician/src/instrument.js
+++ docker/image-musician/src/instrument.js
@@ -5,57 +5,67 @@ var datetime = new Date();
 var protocol = require('./orchestra-protocol');
 
 var dgram = require('dgram');
-var socket = dgram.createSocket('udp4');
+var s = dgram.createSocket('udp4');
 
-// Instrument
-var instrument = process.argv.slice(2);
-console.log('instrument: ', instrument);
 
-var sound;
+function Instrument(instrument){
+    this.instrument = instrument;
 
-switch (instrument){
-case 'piano':
-    console.log('piano');
-    sound = 'ti-ta-ti';
-    break;
-case 'trumpet':
-    console.log('trumpet');
-    sound = 'pouet';
-    break;
-case 'piano':
-    console.log('flute');
-    sound = 'trulu';
-    break;
-case 'violin':
-    console.log('violin');
-    sound = 'gzi-gzi';
-    break;
-case 'drum':
-    console.log('drum');
-    sound = 'boumboum';
-    break;
+    var sound;
+
+    switch (instrument){
+    case 'piano':
+        console.log('piano');
+        sound = 'ti-ta-ti';
+        break;
+    case 'trumpet':
+        console.log('trumpet');
+        sound = 'pouet';
+        break;
+    case 'piano':
+        console.log('flute');
+        sound = 'trulu';
+        break;
+    case 'violin':
+        console.log('violin');
+        sound = 'gzi-gzi';
+        break;
+    case 'drum':
+        console.log('drum');
+        sound = 'boumboum';
+        break;
+    }
+
+    // UUID
+    const UIDGenerator = require('uid-generator');
+    const uidgen = new UIDGenerator(); // Default is a 128-bit UID encoded in base58
+
+    // Sync
+    const UUID = uidgen.generateSync();
+
+    // JSON
+    var json = {
+        'uuid' : UUID,
+        'instrument' : instrument,
+        'activeSince' : datetime
+    };
+
+    // Send datagram
+    const data = Buffer.from(JSON.stringify(json));
+
+    Instrument.prototype.send = function(){
+        s.send(data, 0, data.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, function(err, bytes){
+            console.log("Sending payload: " + data + " via port " + s.address().port);
+        });
+    }
+    setInterval(this.send.bind(this), 1000);
 }
 
-// UUID
-const UIDGenerator = require('uid-generator');
-const uidgen = new UIDGenerator(); // Default is a 128-bit UID encoded in base58
+// Instrument
+var instrument = process.argv[2];
+console.log('instrument is: ', instrument);
 
-// Sync
-const UUID = uidgen.generateSync();
-
-// JSON
-var json = {
-    'uuid' : UUID,
-    'instrument' : instrument,
-    'activeSince' : datetime
-};
-
-// Send datagram
-const data = Buffer.from(JSON.stringify(json));
-
-socket.send(data, 0, data.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS);
-console.log("Send multicast >", data);
-
+var inst = new Instrument(instrument);
 // https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
 // https://www.npmjs.com/package/uid-generator
 
